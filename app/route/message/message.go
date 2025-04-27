76a6a46d0c57e19ed1b4e69097840152a1e6e086
abbie <supermercadoabbie@gmail.com>
2025-04-27 09:29:18 +0800

    Chore: change variable name from cdmsg to cmsg

--- app/route/message/message.go
+++ app/route/message/message.go
@@ -3,12 +3,12 @@ package route
 import (
 	"github.com/gofiber/fiber/v2"
 
-	cdmsg "chatbox/app/controller/message"
+	cmsg "chatbox/app/controller/message"
 
 	hjwt "chatbox/pkg/handler/jwt"
 )
 
 func Route(router fiber.Router) {
-	router.Get("/message", hjwt.ValidateAccessToken, cdmsg.GetMessages)
-	router.Post("/message", hjwt.ValidateAccessToken, cdmsg.SendMessage)
+	router.Get("/message", hjwt.ValidateAccessToken, cmsg.GetMessages)
+	router.Post("/message", hjwt.ValidateAccessToken, cmsg.SendMessage)
 }
